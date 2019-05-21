3e58a9b2524bf55970339e251cef0a8066af6d1e
Khoa <khoanguyen.ptithcm@gmail.com>
2019-05-21 14:53:13 +0700

    Complete CustomAdapter class and create WorkRequirement class

--- app/src/main/java/com/example/work_requires/MainActivity.java
+++ app/src/main/java/com/example/work_requires/MainActivity.java
@@ -5,13 +5,21 @@ import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     //Buttons
     ImageButton menuButton, searchButton;
 
+    //List items
+    List<WorkRequirement> requirementList;
+
     //List View
     ListView requireListView;
+    CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +29,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initialize() {
+        Date today = new Date(System.currentTimeMillis());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
+        String currentDate = dateFormat.format(today.getTime());
         menuButton = findViewById(R.id.menuButton);
         searchButton = findViewById(R.id.searchButton);
         requireListView = findViewById(R.id.requireListView);
+        adapter = new CustomAdapter(MainActivity.this, R.layout.requires_item, requirementList);
+        while()
+        if(requirementList.get(0).getEndDate().equals(currentDate)){
+
+        }
     }
 }
