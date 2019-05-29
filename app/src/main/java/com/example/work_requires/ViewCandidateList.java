f57dde100b6639a193ec23b1182fd00c1c0cb52a
Khoa <khoanguyen.ptithcm@gmail.com>
2019-05-29 09:59:21 +0700

    Sua loi thu tu cot trong file ViewCandidateList

--- app/src/main/java/com/example/work_requires/ViewCandidateList.java
+++ app/src/main/java/com/example/work_requires/ViewCandidateList.java
@@ -55,11 +55,11 @@ public class ViewCandidateList extends AppCompatActivity {
                     cursor.getString(3),cursor.getString(4), cursor.getString(5),cursor.getString(6),
                     cursor.getString(7),cursor.getString(8),cursor.getString(16)));
             dummyUser = userList.get(userList.size()-1);
-            dummyUser.setUser(cursor.getString(10), cursor.getString(11),
-                    cursor.getInt(12), cursor.getString(13),
-                    cursor.getString(14),cursor.getString(15),
-                    cursor.getString(16),cursor.getString(17),
-                    cursor.getString(18), cursor.getString(19));
+            dummyUser.setUser(cursor.getString(9), cursor.getString(10),
+                    cursor.getInt(11), cursor.getString(12),
+                    cursor.getString(13),cursor.getString(14),
+                    cursor.getString(15),cursor.getString(16),
+                    cursor.getString(17), cursor.getString(18));
             userList.set(userList.size()-1,dummyUser);
         }
         setUpRecyclerView();
